06f4eae2119dd587ef1986a5416d750c2a08e249
lockdownumbrela <64942474+lockdownumbrela@users.noreply.github.com>
2020-05-21 17:52:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,17 +3,6 @@ require_once('./classes/router.php');
 require_once('./controller/App.php');
 
 //routes must be here
-/*
-$name =  "one:hello";
-
-$nome = explode(":",$name);
-
-//$obj = new $nome[0]();
-$reflection = new ReflectionMethod($nome[0],$nome[1]);
-$reflection->invoke(new $nome[0]);*/
-
-
-
 
 Router::set('/index.php', "App:home");
 
